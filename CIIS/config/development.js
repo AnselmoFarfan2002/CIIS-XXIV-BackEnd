140a68e3e4d3ec22f3b6634aa36d472223f3eeda
Alvaro Rivera <126705247+alvaro-rivera-ramirez@users.noreply.github.com>
2023-07-18 04:28:50 -0500

    delete private keys

--- CIIS/config/development.js
+++ CIIS/config/development.js
@@ -14,8 +14,8 @@ module.exports = {
     jwt_key: process.env.JWT_PRIVATE_KEY || "C5hCWr@cB<zl6C*]g8sjG7~P@%v9UcXJ",
     recaptcha_key:
       process.env.KEY_RECAPTCHA || "6LeWTiMnAAAAAK1CzdpM9uIxkn3AoM42OZLpySGR",
-    client_id: process.env.CLIENT_ID || "843324626525-s58kork07c87lkb9tcr1u1bqogrl509f.apps.googleusercontent.com",
-    client_secret_key: process.env.CLIENT_SECRET_KEY||"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",
-    refresh_token_email: process.env.REFRESH_TOKEN_EMAIL||'1//04bGyAgLGMnyHCgYIARAAGAQSNwF-L9IrTsRBNQYXRhsFBo0qZxmG14AQWxCdzms2sR1K6JFgS9Tp4MEN6Az9Ez9ewRxOFKVBFi0',
+    client_id: process.env.CLIENT_ID || "",
+    client_secret_key: process.env.CLIENT_SECRET_KEY||"",
+    refresh_token_email: process.env.REFRESH_TOKEN_EMAIL||'',
   },
 };
