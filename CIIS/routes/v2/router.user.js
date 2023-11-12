d4afeae277973a55950166304dd80a26605ef568
AnselmoFarfan2002 <anselmofarfan2002@gmail.com>
2023-11-12 18:20:44 -0500

    2.3.0
    add recovering password

--- CIIS/routes/v2/router.user.js
+++ CIIS/routes/v2/router.user.js
@@ -9,6 +9,11 @@ const TallerInscriptionSQL = require("../../models/Taller/TallerInscription");
 const Taller = require("../../classes/Taller");
 const CONTROLLER_SESSION = require("../../controllers/v2/session");
 const Reservation = require("../../models/Reservation");
+const {
+  mail2sendUserCode,
+  mail2sendUserPass,
+} = require("../../utils/emails/restorePwd");
+const { uuid } = require("uuidv4");
 const routerUser = Router();
 
 routerUser.route("/user").post((req, res) => {
@@ -106,6 +111,85 @@ routerUser.route("/user/password").patch(authMid, async (req, res) => {
       { where: { id_user: req.user.id } }
     );
     res.status(201).json({ msg: "ok" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(http["500"]);
+  }
+});
+
+routerUser.route("/user/code").post(async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    let user = await Users.findOne({
+      where: { email_user: email },
+      attributes: ["name_user", "lastname_user", "code_user"],
+    });
+
+    if (!user?.dataValues)
+      return res.status(404).send({
+        error: "Cuenta no registrada",
+        reason: "Este correo no se encuentra registrado",
+        code: 404,
+      });
+
+    sendMailAtDomain(
+      email,
+      "CIIS SUPPORT - Código único de usuario",
+      mail2sendUserCode({
+        name: user.name_user,
+        lastname: user.lastname_user,
+        code: user.code_user,
+      })
+    );
+
+    res.send(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(http["500"]);
+  }
+});
+
+routerUser.route("/user/restore").post(async (req, res) => {
+  try {
+    const { email, code, dni } = req.body;
+
+    let user = await Users.findOne({
+      where: { email_user: email, code_user: code, dni_user: dni },
+      attributes: ["id_user", "name_user", "lastname_user", "code_user"],
+    });
+
+    if (!user?.dataValues)
+      return res.status(404).send({
+        error: "Algo ocurrió",
+        reason:
+          "No fue posible restaurar la contraseña, revisa los datos especificados",
+        code: 404,
+      });
+
+    let newUserCode = uuid();
+    let pivote = Math.floor(Math.random() * 41);
+    let pass = (await encrypt(newUserCode)).substring(pivote, pivote + 10);
+
+    await Users.update(
+      { code_user: newUserCode, password_user: await encrypt(pass) },
+      {
+        where: { id_user: user.id_user },
+      }
+    );
+
+    sendMailAtDomain(
+      email,
+      "CIIS SUPPORT - Restauración de contraseña",
+      mail2sendUserPass({
+        name: user.name_user,
+        lastname: user.lastname_user,
+        code: newUserCode,
+        pass,
+      })
+    );
+
+    res.send({ msg: "todo ok" });
   } catch (err) {
     console.log(err);
     res.status(500).send(http["500"]);
--- CIIS/utils/http.msg.js
+++ CIIS/utils/http.msg.js
@@ -25,6 +25,11 @@ const http = {
     reason: "Recurso ya existente, surgió un conflicto en el servidor",
     code: 409,
   },
+  200: {
+    msg: "OK",
+    reason: "Operación realizada con éxito",
+    code: 201,
+  },
   201: {
     msg: "Recurso creado",
     reason: "Operación realizada con éxito",
